fb8e87bf9e3fc6cfb7925c6d1b3a4a4fdc7fbfe5
Niclaw <110134480+NiclawYTB@users.noreply.github.com>
2026-05-16 18:18:41 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,24 @@
+import { Manrope, JetBrains_Mono } from 'next/font/google';
 import '../styles/globals.css';
 
+const manrope = Manrope({
+  subsets: ['latin'],
+  variable: '--font-manrope',
+  display: 'swap',
+  weight: ['400', '500', '600', '700'],
+});
+
+const jetbrainsMono = JetBrains_Mono({
+  subsets: ['latin'],
+  variable: '--font-jetbrains-mono',
+  display: 'swap',
+  weight: ['400', '500'],
+});
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <div className={`${manrope.variable} ${jetbrainsMono.variable}`}>
+      <Component {...pageProps} />
+    </div>
+  );
 }
